fcf1ea88cf9756fc96f62545d191a3b2304ee420
kamilu <lucja.kaminska@microsoft.wsei.edu.pl>
2020-07-21 01:12:14 +0200

    class Player 1gra 1player program

---
+++ Oczko/Player.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Oczko
+{
+    public class Player
+    {
+        public string Name { get; set; } = "anonim";
+
+        private List<Card> handCards = new List<Card>();
+        public IReadOnlyList<Card> GetHandCardsList => handCards.AsReadOnly();
+
+        public void PushCard( Card card)
+        {
+            handCards.Add(card);
+        }
+
+        public void PushCard( List<Card> list )
+        {
+            foreach(var card in list)
+            {
+                handCards.Add(card);
+            }
+        }
+
+        public int GetHandCardsValue()
+        {
+            if (handCards.Count == 0)
+                return 0;
+            int sum = 0;
+            foreach( var card in handCards )
+            {
+                sum += card.Value;
+            }
+
+            return sum;
+        }
+    }
+    
+}
